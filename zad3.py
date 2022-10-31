1f2a50f97c558b8a6a3c7935089516a694154ab5
Esco258 <102863166+Esco258@users.noreply.github.com>
2022-10-31 21:34:26 +0100

    Create zad3.py

---
+++ zad3.py
@@ -0,0 +1,8 @@
+def dzielenie(dzielna, dzielnik):
+    if(dzielnik == 0):
+        return "NIE DZIEL PRZEZ ZERO"
+    else:
+        return dzielna / dzielnik
+
+print dzielenie(12, 0)
+print dzielenie(88, 4)
